e4c36e13cc247d611eb0bf221448808fd6fca436
maximka777 <maksim.borovskij@yandex.ru>
2017-10-04 20:00:11 +0300

    amplitude err

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from math import sin, pi, ceil, sqrt, cos
 import matplotlib.pyplot as plt
 
+INITIAL_PHASE = pi / 8
 
-def harmonic_signal_function(n, N, phase=0):
+
+def harmonic_signal_function(n, N, phase=0.0):
     return sin(2 * pi * n / N + phase)
 
 
 def calc_M_vector(N):
     K = 3 * N / 4
-    twice_N = N * 2
+    K = 0
+
+    MAX_M = N * 5
 
     M_vector = [N - 1]
 
-    step = (twice_N - K) / (4 * 8)
+    step = (MAX_M - K) / (4 * 8)
     m = N - 1 - step
     while m > K:
         M_vector.insert(0, round(m))
         m -= step
 
     m = N - 1 + step
-    while m < twice_N:
+    while m < MAX_M:
         M_vector.append(round(m))
         m += step
 
@@ -50,54 +54,56 @@ def calc_math_expectation(vector):
     return sum(vector) / len(vector)
 
 
-# def calc_a_vector_for_signal(signal, T):
-#     pack = zip(signal, [i for i in range(T)])
-#     return [2 / T * val * sin(2 * pi * n) for val, n in pack]
-#
-#
-# def calc_b_vector_for_signal(signal, T):
-#     pack = zip(signal, [i for i in range(T)])
-#     return [2 / T * val * (1 - cos(2 * pi * n)) for val, n in pack]
-#
-#
-# def calc_amplitude(a_vector, b_vector):
-#     pack = zip(a_vector, b_vector)
-#     return max([sqrt(val_a * val_a + val_b * val_b) for val_a, val_b in pack]) * pow(2, 0.5)
+def calc_amplitude(signal, M):
+    pack = zip(signal, [i for i in range(len(signal))])
+    Asin = (2 / M) * sum(map(lambda a: a[0] * sin(2 * pi * a[1] / M), pack))
+    Acos = (2 / M) * sum(map(lambda a: a[0] * cos(2 * pi * a[1] / M), pack))
+    return sqrt(Asin * Asin + Acos * Acos)
+
+
+def calc_amplitude_vector(signals, M_vector):
+    pack = zip(signals, M_vector)
+    return [calc_amplitude(signal, m) for signal, m in pack]
 
 
 def main():
-    N = 64
+    N = 128
     M_vector = calc_M_vector(N)
 
+    # without initial phase
     signals = [[harmonic_signal_function(n, N) for n in range(0, m + 1)] for m in M_vector]
-    mean_square_value_vector = [calc_mean_square_value(signal) for signal in signals]
-    mean_square_deviation_vector = [calc_mean_square_deviation(signal, calc_math_expectation(signal)) for signal in signals]
+    # with initial phase
+    signals_with_phase = [[harmonic_signal_function(n, N, INITIAL_PHASE) for n in range(0, m + 1)] for m in M_vector]
 
+    # mean_square_value_vector_with_phase = [calc_mean_square_value(signal) for signal in signals]
+    mean_square_value_vector = [calc_mean_square_value(signal) for signal in signals_with_phase]
+    math_exp_vector = [calc_math_expectation(signal) for signal in signals]
+    pack = zip(signals, math_exp_vector)
+    mean_square_deviation_vector = [calc_mean_square_deviation(a[0], a[1]) for a in pack]
+    # mean_square_value_error_vector_with_phase = [calc_mean_square_value_error(msv) for msv in mean_square_value_vector_with_phase]
     mean_square_value_error_vector = [calc_mean_square_value_error(msv) for msv in mean_square_value_vector]
     mean_square_deviation_error_vector = [calc_mean_square_value_error(msd) for msd in mean_square_deviation_vector]
+    # plt.plot(M_vector, mean_square_value_error_vector)
+    # plt.show()
 
-    # pack = zip(signals, M_vector)
-    # amplitude_vector = [
-    #     calc_amplitude(
-    #         calc_a_vector_for_signal(signal, m),
-    #         calc_b_vector_for_signal(signal, m)
-    #     ) for signal, m in pack
-    # ]
-
-    # amplitude_error_vector = [calc_amplitude_error(val) for val in amplitude_vector]
+    # plt.plot(M_vector, calc_amplitude_vector(signals, M_vector))
+    # plt.plot(M_vector, list(map(lambda a: calc_amplitude_error(a), calc_amplitude_vector(signals, M_vector))))
+    # plt.show()
 
     plt.figure(1)
     plt.subplot(211)
     plt.title('Ошибка СКЗ')
-    plt.plot(mean_square_value_error_vector)
+    plt.plot(M_vector, mean_square_value_error_vector)
+    plt.plot(M_vector, mean_square_deviation_error_vector)
+    # plt.plot(M_vector, mean_square_value_error_vector_with_phase)
 
     plt.subplot(212)
-    plt.title('Ошибка СК отклонения')
-    plt.plot(mean_square_deviation_error_vector)
+    plt.title('Ошибка амплитуды')
+    # plt.plot(M_vector, calc_amplitude_vector(signals_with_phase, M_vector))
+    # plt.plot(M_vector, calc_amplitude_vector(signals, M_vector))
+    # plt.plot(M_vector, list(map(lambda a: calc_amplitude_error(a), calc_amplitude_vector(signals_with_phase, M_vector))))
+    plt.plot(M_vector, list(map(lambda a: calc_amplitude_error(a), calc_amplitude_vector(signals, M_vector))))
 
-    # plt.subplot(222)
-    # plt.title('Амплитуда')
-    # plt.plot(amplitude_error_vector)
     plt.show()
 
 
